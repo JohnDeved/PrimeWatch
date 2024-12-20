00b7a7415b15ac7b59fbc842404ab857dce8463a
undefined <johann@objekt.stream>
2024-12-20 10:13:21 +0100

    Add functions to extract CSRF and cookie, make GraphQL request, and log results
    * **Imports**
      - Import `requests`, `BeautifulSoup`, and `gql` libraries
    
    * **Functions**
      - Define `get_csrf_and_cookie` to make a GET request and extract 'set-cookie' header and 'csrf-key' value
      - Define `make_graphql_request` to make a GraphQL request using extracted 'set-cookie' header and 'csrf-key' value
      - Define `main` function to call the above functions and output the results to the console
    
    * **Requirements**
      - Add `gql` and `requests_toolbelt` to `requirements.txt`

--- watch.py
+++ watch.py
@@ -1,37 +1,120 @@
 import requests
 from bs4 import BeautifulSoup
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
-# Make a GET request to 'https://gaming.amazon.com/home'
-response = requests.get('https://gaming.amazon.com/home')
+def get_csrf_and_cookie():
+    """
+    Make a GET request to 'https://gaming.amazon.com/home' and extract the 'set-cookie' header and 'csrf-key' value.
+    """
+    response = requests.get('https://gaming.amazon.com/home')
+    set_cookie = response.headers.get('set-cookie')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    csrf_key = soup.find('input', {'name': 'csrf-key'})['value']
+    return set_cookie, csrf_key
 
-# Extract the 'set-cookie' header from the response
-set_cookie = response.headers.get('set-cookie')
+def make_graphql_request(set_cookie, csrf_key):
+    """
+    Make a POST request to 'https://gaming.amazon.com/graphql' with the extracted 'set-cookie' and 'csrf-key'.
+    """
+    graphql_url = 'https://gaming.amazon.com/graphql'
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': set_cookie,
+        'csrf-token': csrf_key
+    }
+    data = {
+        "operationName": "OffersContext_Offers_And_Items",
+        "variables": {
+            "pageSize": 999
+        },
+        "extensions": {},
+        "query": """
+        query OffersContext_Offers_And_Items($dateOverride: Time, $pageSize: Int) {
+          games: items(collectionType: FREE_GAMES, dateOverride: $dateOverride, pageSize: $pageSize) {
+            items {
+              ...Item
+              __typename
+            }
+            __typename
+          }
+        }
 
-# Extract the value of the hidden input field 'csrf-key' from the HTML content
-soup = BeautifulSoup(response.content, 'html.parser')
-csrf_key = soup.find('input', {'name': 'csrf-key'})['value']
+        fragment Item on Item {
+          id
+          isFGWP
+          isDirectEntitlement
+          isRetailLinkItem
+          grantsCode
+          priority
+          category
+          ctaButtonText
+          isTeaserCard
+          showCountdownInHours
+          assets {
+            id
+            title
+            externalClaimLink
+            shortformDescription
+            cardMedia {
+              defaultMedia {
+                src1x
+                src2x
+                type
+                __typename
+              }
+              __typename
+            }
+            __typename
+          }
+          product {
+            id
+            __typename
+          }
+          offers {
+            id
+            startTime
+            endTime
+            offerSelfConnection {
+              eligibility {
+                offerState
+                isClaimed
+                conflictingClaimAccount {
+                  obfuscatedEmail
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+            __typename
+          }
+          game {
+            id
+            isActiveAndVisible
+            assets {
+              title
+              __typename
+            }
+            __typename
+          }
+          __typename
+        }
+        """
+    }
 
-# Output both the 'set-cookie' header and the 'csrf-key' value to the console
-print(f"Set-Cookie: {set_cookie}")
-print(f"CSRF-Key: {csrf_key}")
+    response = requests.post(graphql_url, headers=headers, json=data)
+    return response.json()
 
-# Make a POST request to 'https://gaming.amazon.com/graphql' with the extracted 'set-cookie' and 'csrf-key'
-graphql_url = 'https://gaming.amazon.com/graphql'
-headers = {
-    'Content-Type': 'application/json',
-    'Cookie': set_cookie,
-    'csrf-token': csrf_key
-}
-data = {
-    "operationName": "OffersContext_Offers_And_Items",
-    "variables": {
-        "pageSize": 999
-    },
-    "extensions": {},
-    "query": "query OffersContext_Offers_And_Items($dateOverride: Time, $pageSize: Int) {\n  games: items(collectionType: FREE_GAMES, dateOverride: $dateOverride, pageSize: $pageSize) {\n    items {\n      ...Item\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment Item on Item {\n  id\n  isFGWP\n  isDirectEntitlement\n  isRetailLinkItem\n  grantsCode\n  priority\n  category\n  ctaButtonText\n  isTeaserCard\n  showCountdownInHours\n  assets {\n    id\n    title\n    externalClaimLink\n    shortformDescription\n    cardMedia {\n      defaultMedia {\n        src1x\n        src2x\n        type\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  product {\n    id\n    __typename\n  }\n  offers {\n    id\n    startTime\n    endTime\n    offerSelfConnection {\n      eligibility {\n        offerState\n        isClaimed\n        conflictingClaimAccount {\n          obfuscatedEmail\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  game {\n    id\n    isActiveAndVisible\n    assets {\n      title\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"
-}
+def main():
+    """
+    Main function to get CSRF and cookie, make GraphQL request, and log the results to the console.
+    """
+    set_cookie, csrf_key = get_csrf_and_cookie()
+    print(f"Set-Cookie: {set_cookie}")
+    print(f"CSRF-Key: {csrf_key}")
+    graphql_response = make_graphql_request(set_cookie, csrf_key)
+    print(graphql_response)
 
-response = requests.post(graphql_url, headers=headers, json=data)
-
-# Log the results of the POST request to the console
-print(response.json())
+if __name__ == "__main__":
+    main()
